7425da02bc19bda702c3a11df42d55db0c5c7771
Stephen Miller <sdmiller@eecs.berkeley.edu>
2010-08-25 18:42:59 +0000

    Removed unnecessary call to Undistort, commented birdseye

--- visual_feedback/snapshotter/src/birdseye_filter.py
+++ visual_feedback/snapshotter/src/birdseye_filter.py
@@ -23,38 +23,34 @@ class BirdseyeFilter(SnapshotFilter):
         self.height = rospy.get_param("~init_height",-70)
     
     def image_filter(self,cv_image,info,copy=None):
+        #Get the width and height of the board
         board_w = self.cols
         board_h = self.rows
+        #Area of the board = "board_n"
         board_n  = board_w * board_h
         board_sz = (board_w,board_h)
+        #Pulls the intrinsic matrix, in cvMat form, from the CameraInfo message received by the camera.
         intrinsic = self.intrinsic_matrix_from_info(info)
-        distortion = self.dist_coeff_from_info(info)
         image = cv_image
+        #This needs to be fed with a "height", so it knows how high up the perspective transform should be
         init_height = self.height
+        #Only works on a grayscale image
         gray_image = cv.CreateImage(cv.GetSize(image),8,1)
         cv.CvtColor(image, gray_image, cv.CV_BGR2GRAY)
         
-        #Undistort Image
-        mapx = cv.CreateImage( cv.GetSize(image), cv.IPL_DEPTH_32F, 1 )
-        mapy = cv.CreateImage( cv.GetSize(image), cv.IPL_DEPTH_32F, 1 )
-        cv.InitUndistortMap(  
-            intrinsic,  
-            distortion,  
-            mapx,  
-            mapy  
-            )
-        t = CloneImage(image)
-        cv.Remap( t, image, mapx, mapy )
-        corner_count = 0
+        #Uses openCV to find the checkerboard
         (found,corners) = cv.FindChessboardCorners(image,board_sz, (cv.CV_CALIB_CB_ADAPTIVE_THRESH | cv.CV_CALIB_CB_FILTER_QUADS))
         if(not found):
-            print "Couldn't aquire checkerboard, only found %d of %d corners\n"%(corner_count,board_n)
+            print "Couldn't aquire checkerboard, only found 0 of %d corners\n"%board_n
             gr = CloneImage(image)
             cv.CvtColor(gray_image,gr,cv.CV_GRAY2BGR)
             return gr
+        #We need subpixel accuracy, so we tell it where the corners are and it magically does the rest. I forget what (11,11) and (-1,-1) mean.
         cv.FindCornerSubPix(gray_image, corners,    
               (11,11),(-1,-1),   
               ( cv.CV_TERMCRIT_EPS+cv.CV_TERMCRIT_ITER, 30, 0.1 ))
+        #Pull out the Object Points (3d location of the checkerboard corners, in the camera frame)
+        #and the Image Points (2d location of the corners in the actual image) 
         objPts = point_array(4)
         imgPts = point_array(4)
         objPts[0] = (0,0)
@@ -65,14 +61,17 @@ class BirdseyeFilter(SnapshotFilter):
         imgPts[1] = corners[board_w-1]
         imgPts[2] = corners[(board_h-1)*board_w]
         imgPts[3] = corners[(board_h-1)*board_w + board_w - 1]
+        
+        #Use GetPerspectiveTransform to populate our Homography matrix
         H = cv.CreateMat( 3, 3, cv.CV_32FC1)
-        print H
         cv.GetPerspectiveTransform(objPts,imgPts,H)
         birds_image = CloneImage(image)
+        #Since we don't get any z information from this, we populate H[2,2] with our hard-coded height
         H[2,2] = init_height
+        #Uses the homography matrix to warp the perspective.
         cv.WarpPerspective(image,birds_image,H,  
             cv.CV_INTER_LINEAR+cv.CV_WARP_INVERSE_MAP+cv.CV_WARP_FILL_OUTLIERS )
-        print birds_image
+        #Note: If you need to undo the transformation, you can simply invert H and call cv.WarpPerspective again.
         return birds_image
         
     def intrinsic_matrix_from_info(self, cam_info):
@@ -84,10 +83,6 @@ class BirdseyeFilter(SnapshotFilter):
            intrinsic_matrix[i, j] = cam_info.P[4*i+j]
        return intrinsic_matrix
        
-    def dist_coeff_from_info(self,cam_info):
-        dist_coeff = cv.CreateMat(1, 4, cv.CV_32FC1)
-        cv.Set(dist_coeff,0.0)
-        return dist_coeff
 
 
 def point_array(length):
